6f822939da219f23fe9ef0be0b5e2150066138f0
galaxygoldfish <shriscu6340@gmail.com>
2022-02-26 15:44:19 -0800

    [Feature] Add compose navigation library & setup

--- app/src/main/java/com/pineapple/app/MainActivity.kt
+++ app/src/main/java/com/pineapple/app/MainActivity.kt
@@ -3,15 +3,45 @@ package com.pineapple.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.pineapple.app.theme.PineappleTheme
+import com.pineapple.app.view.WelcomeView
+
+object NavDestination {
+    const val WelcomeView = "welcome"
+}
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var navigationController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PineappleTheme {
-
+                NavigationHost()
             }
         }
     }
+
+
+    @Composable
+    @OptIn(ExperimentalAnimationApi::class)
+    fun NavigationHost() {
+        navigationController = rememberAnimatedNavController()
+        NavHost(
+            navController = navigationController,
+            startDestination = NavDestination.WelcomeView
+        ) {
+            composable(NavDestination.WelcomeView) {
+                WelcomeView(navController = navigationController)
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/pineapple/app/view/WelcomeView.kt
@@ -0,0 +1,14 @@
+package com.pineapple.app.view
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun WelcomeView(navController: NavController) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
